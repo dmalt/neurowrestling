bd000ddb19876f6e094dd8ba67c70c60aa026142
Dmitriy Altukhov <dm.altukhov@ya.ru>
2017-10-10 15:57:06 +0300

    move imoprts to please linter

--- booze_car.py
+++ booze_car.py
@@ -8,18 +8,18 @@ Mon Jun 26 14:34:41 MSK 2017
 
 """
 
+from time import sleep
+from collections import deque
 import rpyc
 from pylsl import resolve_byprop, StreamInlet
 from numpy import sign
-from time import sleep
-from collections import deque
 
 LSL_TIMEPROP = 5
 
-conn = rpyc.classic.connect('169.254.23.59') # host name or IP address of the EV3
+conn = rpyc.classic.connect('10.42.0.3') # host name or IP address of the EV3
 
 ev3 = conn.modules['ev3dev.ev3']      # import ev3dev.ev3 remotely
- # ev3.Sound.speak('Welcome to the E V 3 dev project!').wait()
+# ev3.Sound.speak('Sasha privet').wait()
 
 def rotate_90(l, r, speed):
      l.run_timed(time_sp=10000, speed_sp=speed)
@@ -55,19 +55,23 @@ def count_nums(array, code_l, code_r):
 class CustomException(Exception):
     def __init__(self, value):
         self.parameter = value
+
     def __str__(self):
         return repr(self.parameter)
+
 
 if __name__ == '__main__':
     # ---------- init servo motor ---------------- #
     servo = ev3.MediumMotor('outB')
     servo.run_to_abs_pos(position_sp=0, speed_sp=50)
+    # servo.position_sp = 0
+    # servo.reset()
     # -------------------------------------------- #
 
-    win_len = 100;
+    win_len = 50;
 
-    pl1 = 'sasha'
-    pl2 = 'kolya'
+    pl1 = 'sosna'
+    pl2 = 'lipa'
 
     # pl1 = 'bad'
     # pl2 = 'bad1'
@@ -88,9 +92,11 @@ if __name__ == '__main__':
     stream_pl1 = resolve_byprop('name', pl1, timeout=LSL_TIMEPROP)
     stream_pl2 = resolve_byprop('name', pl2, timeout=LSL_TIMEPROP)
     # ----------------------------------------------------------- #
-    if not(stream_pl1 and stream_pl2):
-        raise CustomException('STREAM CONNECTION ERROR')
+    if not(stream_pl1):
+        raise CustomException('STREAM1 CONNECTION ERROR')
 
+    if not(stream_pl2):
+        raise CustomException('STREAM2 CONNECTION ERROR')
 
     pl1_inlet = StreamInlet(stream_pl1[0])
     pl2_inlet = StreamInlet(stream_pl2[0])
@@ -99,7 +105,13 @@ if __name__ == '__main__':
 
     while(True):
         pl1_chunk = pl1_inlet.pull_chunk()[0]
+        print('1-st players chunk')
+        print(pl1_chunk)
+        print('-------------------------')
         pl2_chunk = pl2_inlet.pull_chunk()[0]
+        print('2-st players chunk')
+        print(pl2_chunk)
+        print('_________________________')
 
         pl1_flat = [i for sublist in pl1_chunk for i in sublist]
         pl2_flat = [i for sublist in pl2_chunk for i in sublist]
@@ -127,6 +139,7 @@ if __name__ == '__main__':
             servo.run_forever(speed_sp=speed)
 
         print('position = {}'.format(servo.position))
+
         if abs(servo.position) > amplitude:
             servo.stop()
             sleep(3)
