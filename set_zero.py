ada65c6bd6a4abdac1d7c48cead1bf901dda1bb0
Dmitriy Altukhov <dm.altukhov@ya.ru>
2018-01-06 15:05:46 +0300

    fix style

--- set_zero.py
+++ set_zero.py
@@ -1,20 +1,22 @@
+"""Setup servo's zero position"""
+
 import curses
 from setup_brick import servo
 
 
 if __name__ == '__main__':
 
-    sp = 2 # rotation step
+    sp = 2  # rotation step
 
     # get the curses screen window
     screen = curses.initscr()
-     
+
     # turn off input echoing
     curses.noecho()
-     
+
     # respond to keys immediately (don't wait for enter)
     curses.cbreak()
-     
+
     # map arrow keys to special values
     screen.keypad(True)
     cur_pos = '  =============  '
@@ -22,7 +24,6 @@ if __name__ == '__main__':
 | q - quit                         |\n\
 | s - set current position as zero |\n\
 | h - run to current zero          |\n'
-
 
     try:
         while True:
@@ -46,9 +47,11 @@ if __name__ == '__main__':
                 servo.run_to_rel_pos(position_sp=sp * 20, speed_sp=150)
                 servo.wait_while('running')
             elif char == ord('j'):
-                servo.run_to_rel_pos(position_sp= - sp * 20, speed_sp=150)
+                servo.run_to_rel_pos(position_sp=- sp * 20, speed_sp=150)
                 servo.wait_while('running')
     finally:
         # shut down cleanly
-        curses.nocbreak(); screen.keypad(0); curses.echo()
+        curses.nocbreak()
+        screen.keypad(0)
+        curses.echo()
         curses.endwin()
